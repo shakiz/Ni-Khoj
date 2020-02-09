b8acb0bff8f702f4e29547775923aad3a7da0928
shakiz <activities335@gmail.com>
2020-02-09 21:01:52 +0600

    lets beginnnn

---
+++ app/src/main/java/com/shakil/ni_khoj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shakil.ni_khoj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
