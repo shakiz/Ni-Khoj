a30f5bb9979164767e5a9300d80aaa3257eada63
shakiz <activities335@gmail.com>
2020-02-13 00:59:32 +0600

    add post dialog and search layout

--- app/src/main/java/com/shakil/ni_khoj/fragments/HomeFragment.java
+++ app/src/main/java/com/shakil/ni_khoj/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.shakil.ni_khoj.fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,6 +10,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import com.shakil.ni_khoj.NewsFeedAdapter;
 import com.shakil.ni_khoj.R;
 import com.shakil.ni_khoj.utils.DataDump;
@@ -16,6 +21,9 @@ import com.shakil.ni_khoj.utils.DataDump;
 public class HomeFragment extends Fragment {
     private DataDump dataDump;
     private RecyclerView recyclerView;
+    private ImageView newPost;
+    private Dialog itemDialog;
+    private RelativeLayout dialogLayout;
 
     public HomeFragment() {
     }
@@ -36,14 +44,38 @@ public class HomeFragment extends Fragment {
     private void init(View view) {
         dataDump = new DataDump(getContext());
         recyclerView = view.findViewById(R.id.mRecyclerView);
+        newPost = view.findViewById(R.id.newPost);
     }
 
     private void bindUiWithComponents() {
+        setAdapter();
 
+        newPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void setAdapter() {
         NewsFeedAdapter newsFeedAdapter = new NewsFeedAdapter(dataDump.dumpNewsFeedData(), getContext());
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(newsFeedAdapter);
         newsFeedAdapter.notifyDataSetChanged();
     }
 
+    private void showDialog() {
+        itemDialog = new Dialog(getContext());
+        itemDialog.setContentView(R.layout.dialog_popup_new_post);
+        customViewInit(itemDialog);
+        itemDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        Animation a = AnimationUtils.loadAnimation(itemDialog.getContext(), R.anim.push_up_in);
+        dialogLayout.startAnimation(a);
+        itemDialog.show();
+    }
+
+    private void customViewInit(Dialog itemDialog) {
+        dialogLayout = itemDialog.findViewById(R.id.dialogLayout);
+    }
 }
